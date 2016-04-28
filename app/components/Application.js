040ec80d935ed097d1d3a5bde67b8344b4576fd2
David Bazile <dbazile@radiantblue.com>
2016-04-28 15:21:31 -0400

    Introduce concept of multiple durations

--- app/components/Application.js
+++ app/components/Application.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react'
 import MapView from './MapView'
 import GeoWaveLogo from './GeoWaveLogo'
-import Duration from './Duration'
+import DurationList from './DurationList'
 import styles from './Application.css'
 
 export default class Application extends Component {
   constructor() {
     super()
-    this.state = {duration: null, origin: {lat: 40.78, lng: -73.96}, destination: {lat: 40.65, lng: -73.78}}
+    this.state = {durations: [], origin: {lat: 40.78, lng: -73.96}, destination: {lat: 40.65, lng: -73.78}}
     this._destinationChanged = this._destinationChanged.bind(this)
     this._originChanged = this._originChanged.bind(this)
     this._fetchDurations = debounce(this._fetchDurations.bind(this), 500)
@@ -25,7 +25,7 @@ export default class Application extends Component {
                  originChanged={this._originChanged}
                  destinationChanged={this._destinationChanged}>
         </MapView>
-        {this.state.duration && <Duration seconds={this.state.duration}/>}
+        <DurationList durations={this.state.durations}/>
         <footer className={styles.footer}>
           <GeoWaveLogo/>
         </footer>
@@ -40,11 +40,11 @@ export default class Application extends Component {
   _fetchDurations() {
     const {origin, destination} = this.state
     const now = new Date().toISOString()
+    this.setState({durations: []})
     fetch(`/nyc-taxi/tripInfo?startLat=${origin.lat}&startLon=${origin.lng}&destLat=${destination.lat}&destLon=${destination.lng}&startTime=${now}`)
       .then(response => response.json())
-      .then(([[, duration]]) => {
-        console.debug('travel times:', duration)
-        this.setState({duration})
+      .then(durations => {
+        this.setState({durations})
       })
   }
 
--- app/components/Duration.js
+++ app/components/Duration.js
@@ -6,20 +6,23 @@ const TIME_FORMAT = 'h:mma'
 
 export default class Duration extends Component {
   render() {
-    const now = moment()
-    const future = moment().add(this.props.seconds, 'seconds')
+    const [timestamp, seconds] = this.props.duration
+    const now = moment(timestamp, moment.ISO_8601)
+    const future = moment().add(seconds, 'seconds')
     return (
-      <div className={styles.root}>
-        <h2 className={styles.duration}>~{future.fromNow(true)}</h2>
-        <div className={styles.timestamps}>
-          <span className={styles.now}>{now.format(TIME_FORMAT)}</span>
-          <span className={styles.then}>{future.format(TIME_FORMAT)}</span>
-        </div>
-      </div>
+      <li className={styles.root}>
+        <span className={styles.duration}>~{future.fromNow(true)}</span>
+        <span className={styles.timestamp}>
+          <span className={styles.label}>Departing At</span>
+          <svg className={styles.clock} viewBox="1 1 98 98">
+            <path d="M52,48.7046998 L52,11 L48,11 L48,51 L48.0081411,51 L48,51.0141009 L77.2977379,67.9291577 L79.2977379,64.4650561 L52,48.7046998 Z M50,99 C77.0619527,99 99,77.0619527 99,50 C99,22.9380473 77.0619527,1 50,1 C22.9380473,1 1,22.9380473 1,50 C1,77.0619527 22.9380473,99 50,99 Z M50.5,93 C73.9721019,93 93,73.9721019 93,50.5 C93,27.0278981 73.9721019,8 50.5,8 C27.0278981,8 8,27.0278981 8,50.5 C8,73.9721019 27.0278981,93 50.5,93 Z" fillRule="evenodd"/>
+          </svg>
+          {now.format(TIME_FORMAT)}</span>
+      </li>
     )
   }
 }
 
 Duration.propTypes = {
-  seconds: React.PropTypes.number.isRequired
+  duration: React.PropTypes.object.isRequired
 }
