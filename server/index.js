00584baf439a4ba3a773df14e58f762c2b08671a
David Bazile <dbazile@radiantblue.com>
2016-04-28 10:39:21 -0400

    Add mock support for time intervals

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@
 const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
+const moment = require('moment')
 
 const app = express()
 
@@ -11,8 +12,16 @@ app.use('/nyc-taxi/shapes', express.static(path.join(__dirname, 'data')))
 app.use(morgan('dev'))
 
 app.get('/nyc-taxi/tripInfo', (req, res) => {
-  console.log(JSON.stringify(req.query))
-  res.send([[new Date(), Math.ceil(Math.random() * 600)]])
+  const start = moment(req.query.startTime, moment.ISO_8601)
+    .minutes(Math.round(moment().minutes() / 15) * 15)
+    .seconds(0)
+  console.log('%s -> %s', start, JSON.stringify(req.query))
+  res.send([
+    [moment(start).add(0, 'minutes'), Math.ceil(Math.random() * 600)],
+    [moment(start).add(15, 'minutes'), Math.ceil(Math.random() * 700)],
+    [moment(start).add(30, 'minutes'), Math.ceil(Math.random() * 800)],
+    [moment(start).add(45, 'minutes'), Math.ceil(Math.random() * 900)],
+  ])
 })
 
 console.log('Listening on port 8079')
